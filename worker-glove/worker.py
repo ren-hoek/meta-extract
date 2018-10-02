c20676cb5403586edf604ade897d2495cb275ad4
jjfallon <joejf25@hotmail.com>
2018-10-02 16:40:07 +0100

    Ideas for extensions to worker for doc2vec

--- worker-glove/worker.py
+++ worker-glove/worker.py
@@ -7,9 +7,12 @@ from rq import Worker, Queue, Connection
 
 # Load the spacy model once per container
 model_path = '/data/spacy/en_core_web_lg/en_core_web_lg/en_core_web_lg-2.0.0/'
+dv.fix_bug(dv.model)
+#dv.model = spacy.load(model_path, disable=['ner'])
 dv.model = spacy.load(model_path)
 dv.fix_bug(dv.model)
-
+dv.fix_bug(dv.model)
+#dv.model.max_length = 2000000
 
 listen = ['high', 'default', 'low']
 
